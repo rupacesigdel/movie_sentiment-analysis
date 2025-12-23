c5a034f516833016fe7e1dfabad93a7e7fea0a7c
Rupesh Sigdel <rupeshcgdl2060@gmail.com>
2025-12-23 13:43:25 +0545

    Added clean_text

--- src/data_utils.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def clean_text(text):
-    text = text.lower()                  
-    text = re.sub(r"<.*?>", "", text)     
-    text = re.sub(r"[^a-zA-Z\s]", "", text) 
-    text = re.sub(r"\s+", " ", text)      
-    return text.strip()
